707640a7f25303c3c2092de388f8c01455ff9e49
Dave Locke <34027679+lockedj@users.noreply.github.com>
2021-05-11 19:26:05 +0100

    Calculate end time and tidy up

--- battery.py
+++ battery.py
@@ -7,6 +7,8 @@ from datetime import datetime
 import os
 import sys
 import getopt
+import math
+from datetime import time, timedelta, datetime, date
 
 # todo - update NAS to run daily
 # todo - web site to show output
@@ -38,9 +40,11 @@ class Battery:
                 "daylight", "towinter", fallback=16)
             self.preCharge = config.getint("battery", "mincharge", fallback=30)
             self.hourlycharge = config.getint(
-                "battery", "hourlycharge", fallback=2)
+                "battery", "solarhourlycharge", fallback=2)
+            self.gridhourlycharge = config.getfloat(
+                "battery", "gridhourlycharge", fallback=2.5)
             self.maxcharge = config.getfloat(
-                "battery", "maxcharge", fallback=7.8)
+                "battery", "maxcharge", fallback=7.0)
             self.houseuse = config.get(
                 "battery", "houseuse").split(",")
             self.cheapRateFrom = config.get(
@@ -55,6 +59,31 @@ class Battery:
                 "Problem reading battery config file"+self.configdir+"battery.conf")
         return
 
+    def determineEndTime(self, precharge):
+        # Work out end time to stop pre-charging the battery - no need using more
+        # from grid than needed
+        chargekwh = (precharge/100) * self.maxcharge
+        timerequired = chargekwh / self.gridhourlycharge
+        addhours = math.floor(timerequired)
+        remaintime = timerequired-addhours
+        addmins = 0
+        if remaintime >= 0 and remaintime < .25:
+            addmins = 15
+        elif remaintime >= .25 and remaintime < .50:
+            addmins = 30
+        elif remaintime >= .50 and remaintime < .75:
+            addmins = 45
+        else:
+            addhours = addhours+1
+
+        starttime = time(
+            hour=int(self.cheapRateFrom[0:2]), minute=int(self.cheapRateFrom[2:4]))
+        startdate = datetime.combine(date.today(), starttime)
+        td = timedelta(seconds=(addhours*60*60)+(addmins*60))
+        enddate = startdate+td
+        endtime = enddate.strftime('%H%M')
+        return endtime
+
     def determinePreCharge(self):
         now = datetime.today()
         month = now.month
@@ -68,37 +97,42 @@ class Battery:
         forecast = weather.Weather()
         clouds = forecast.cloudTomorrow()
 
-        hr = 0
-        use = 0
-        gen = 0
-        highuse = 0
-        charge = 0
+        hr = 0          # hour of the day being processed
+        use = 0         # house use + battery charge
+        gen = 0         # how much generated in selected hour
+        highcharge = 0  # Highest kWh battery will get to
+        charge = 0      # Min kWh to pre-charge the battery
 
+        # First calculate if there is enough forecast solar radiation to both cover
+        # basic house use and to charge the battery.
         while hr < 24:
             use = use - float(self.houseuse[hr])    # Subtract house use
-            if hr >= cloudfrom and hr <= cloudto:   # If daylight houg
+            if hr >= cloudfrom and hr <= cloudto:   # If daylight hour
                 if clouds[hr] < 100:
                     gen = self.hourlycharge*((100-clouds[hr])/100)
-                    use = use + gen  # add in generation
             else:
-                gen = 0
-            if use < charge:                        # Remember max bat charge required
-                charge = use
-            if use > highuse:                       # Remember high use
-                highuse = use
+                gen = 0                             # no daylight so no gen
+            use = use + gen                         # total of house use + generated
+
+            if use < charge:                        # If demand is greater than current precharge
+                charge = use                        # set pre-charge to demand
+            if use > highcharge:                       # Remember high use
+                highcharge = use
             if -charge > self.maxcharge:            # If precharge needed is more than
-                charge = -int(self.maxcharge)       # battery capacity then bat
+                # set precharge to max capacity
+                charge = -int(self.maxcharge)
                 hr = hr+1
                 break                               # needs max charge
-            if (highuse - charge) >= self.maxcharge:  # If gen more than capacity then
+            if (highcharge - charge) >= self.maxcharge:  # If gen more than capacity then
                 hr = hr+1
                 break                               # stop as precharge known
             logging.getLogger().info(
-                f"hour {hr} cloudcvr {clouds[hr]} use {use:0.2f} precharge {charge:0.2f} gen{gen} high {highuse}")
+                f"hour {hr} cloudcvr {clouds[hr]} use {use:0.2f} precharge {charge:0.2f} gen {gen:0.2f} high {highcharge:0.2f}")
             hr = hr+1
 
-        # calculate additional solar capacity for use by dishwasher..
-        spare = 0
+        # calculate additional solar capacity over and above what is needed
+        # by the battery. This can then be used for other devices like the dishwasher..
+        spare = 0       # additional solar capacity over and above needs of battery
         while hr < 24:
             use = use - float(self.houseuse[hr])    # Subtract house use
             if hr >= cloudfrom and hr <= cloudto:   # If daylight houg
@@ -106,7 +140,7 @@ class Battery:
                     gen = self.hourlycharge*((100-clouds[hr])/100)
                     spare = spare + gen                 # add in generation
             logging.getLogger().info(
-                f"hour {hr} cloudcvr {clouds[hr]} spare {spare} gen{gen}")
+                f"hour {hr} cloudcvr {clouds[hr]} spare {spare:0.2f} gen{gen:0.2f}")
             hr = hr+1
 
         charge = -charge
@@ -143,6 +177,7 @@ def main(argv):
     try:
         battery = Battery(configdir)
         charge = battery.determinePreCharge()
+        chargeToTime = battery.determineEndTime(charge)
 
         # Update the giv control system with charge volume.  If on dev machine use local web driver,
         # if on NAS then need to use remote web driver to access the docker
@@ -153,7 +188,7 @@ def main(argv):
         else:
             giv.setChromeDriverRemote()
         giv.configBatteryCharge(battery.cheapRateFrom,
-                                battery.cheapRateTo, charge)
+                                chargeToTime, charge)
     except:
         logger.exception("giv update failed")
         raise
